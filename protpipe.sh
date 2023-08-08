dc2f9b38668be910684dd7ff8c1e940c8ac43aff
Cory Weller <cory.a.weller@gmail.com>
2023-08-08 09:09:42 -0400

    rename wrapper and update filename

--- protpipe.sh
+++ protpipe.sh
@@ -19,7 +19,6 @@ else
     echo 'INFO: singularity command found'
 fi
 
-library://wellerca/protpipe/4.3.1:sha256.523df8eb1aa5c9eb30ac5cd70c3181431f90af6e12e497ee6d43854e5f578196
 
 #### R ANALYSIS ####################################################################################
 r_sif="src/R.sif"
@@ -27,7 +26,7 @@ r_sif_sha256_desired='523df8eb1aa5c9eb30ac5cd70c3181431f90af6e12e497ee6d43854e5f
 if [ ! -f "${r_sif}" ]; then
     echo "INFO: pulling image from Singularity cloud"
     singularity pull --arch amd64 library://wellerca/protpipe/4.3.1:sha256.523df8eb1aa5c9eb30ac5cd70c3181431f90af6e12e497ee6d43854e5f578196
-    mv 4.0_sha256.523df8eb1aa5c9eb30ac5cd70c3181431f90af6e12e497ee6d43854e5f578196.sif "${r_sif}"
+    mv 4.3.1_sha256.523df8eb1aa5c9eb30ac5cd70c3181431f90af6e12e497ee6d43854e5f578196.sif "${r_sif}"
 else
     echo "INFO: ${r_sif} already exists, skipping download"
 fi
@@ -41,5 +40,6 @@ else
     echo "INFO: ${r_sif} sha256sum sum passes check"
 fi
 
-r_processing_script='src/counts_processing.R'
-singularity exec --cleanenv -H ${PWD} ${r_sif} Rscript ${r_processing_script} $@
+#### RUN IN CONTAINER ####################################################################################
+
+singularity exec --cleanenv -H ${PWD} ${r_sif} Rscript $@
